d096e16af5a02e5595b64cee1ba44701c92630f1
mel418 <melodygatan@gmail.com>
2025-02-16 22:46:08 -0800

    wait they dont love you like i love you

---
+++ FileCopy/README.md
@@ -0,0 +1,2 @@
+to compile: gcc -o filecopy filecopy.c
+to run: ./filecopy input.txt copy.txt
--- FileCopy/filecopy.c
+++ FileCopy/filecopy.c
@@ -1,63 +1,50 @@
-/**
- * filecopy.c
- * 
- * This program copies files using a pipe.
- *
- */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
- #include <complex.h>
- #include <unistd.h>
- #include <stdio.h>
- #include <stdlib.h>
- #include <fcntl.h>
- #include <sys/types.h>
-//  #include <sys/wait.h>
- #include <string.h>
- 
- #define READ_END 0
- #define WRITE_END 1
- 
- int main(int argc, char *argv[])
- {
-     /* 
-     Your code is here
-     */
-    // check for correct amount of command-line arguments
-    if(argc != 3) {
-        printf("Error: wrong amount of arguments\n Example: $ ./filecopy sourceFile destinationFile\n");
+#define READ_END 0
+#define WRITE_END 1
+
+int main(int argc, char *argv[])
+{
+    // Check for correct number of arguments
+    if (argc != 3) {
+        printf("Error: wrong number of arguments\nExample: $ ./filecopy sourceFile destinationFile\n");
         return EXIT_FAILURE;
     }
 
-    // open the source file to read only
+    // Open the source file for reading
     int inFile = open(argv[1], O_RDONLY);
     if (inFile < 0) {
-        printf("Error opening source file");
+        perror("Error opening source file");
         return EXIT_FAILURE;
     }
 
-    // open the destination file to write and to create if doesnt exist
-    int outFile = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);  // O_TRUNC: Truncate the file to zero length if it already exists 
+    // Open the destination file for writing
+    int outFile = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (outFile < 0) {
-        printf("Error opening/creating destination file");
+        perror("Error opening/creating destination file");
         close(inFile);
         return EXIT_FAILURE;
     }
-    
-    // array of two ints: pipefd[0] → Read end of the pipe | pipefd[1] → Write end of the pipe
-    int pipefd[2]; 
 
-    // create pipe: communication channel between two processes (parent and child)
+    // Create a pipe for inter-process communication
+    int pipefd[2];
     if (pipe(pipefd) < 0) {
-        printf("Pipe creation failed");
+        perror("Pipe creation failed");
         close(inFile);
         close(outFile);
         return EXIT_FAILURE;
     }
 
-    // fork creates a new child process
+    // Fork to create a child process
     pid_t pid = fork();
     if (pid < 0) {
-        printf("Fork failed");
+        perror("Fork failed");
         close(inFile);
         close(outFile);
         close(pipefd[READ_END]);
@@ -65,19 +52,53 @@
         return EXIT_FAILURE;
     }
 
-    // child process: only reads from the pipe
-    if (pid == 0) {
-        close(pipefd[WRITE_END]); // close the write end of the pipe
+    // Parent process: write from the source file to the pipe
+    if (pid > 0) {
+        close(pipefd[READ_END]); // Close the read end of the pipe
+        char buffer[1024];
+        ssize_t bytesRead;
 
-        char buffer[1024]; // buffer to hold data
-        ssize_t bytes_read; // stores the number of bytes read
-
-        while ((bytes_read = read(pipefd[READ_END], buffer, sizeof(buffer))) > 0) {
-            if (write(outFile, buffer, bytes_read) != bytes_read) {
-                printf("Error");
+        // Read from the source file and write to the pipe
+        while ((bytesRead = read(inFile, buffer, sizeof(buffer))) > 0) {
+            if (write(pipefd[WRITE_END], buffer, bytesRead) != bytesRead) {
+                perror("Write to pipe failed");
+                close(inFile);
+                close(outFile);
+                close(pipefd[WRITE_END]);
+                return EXIT_FAILURE;
             }
         }
+
+        // Close the write end of the pipe and source file
+        close(pipefd[WRITE_END]);
+        close(inFile);
+
+        // Wait for the child process to finish
+        waitpid(pid, NULL, 0);
+
+        // Print success message only in the parent process after child finishes
+        printf("File successfully copied from %s to %s.\n", argv[1], argv[2]);
+    }
+    // Child process: read from the pipe and write to the destination file
+    else {
+        close(pipefd[WRITE_END]); // Close the write end of the pipe
+        char buffer[1024];
+        ssize_t bytesRead;
+
+        // Read from the pipe and write to the destination file
+        while ((bytesRead = read(pipefd[READ_END], buffer, sizeof(buffer))) > 0) {
+            if (write(outFile, buffer, bytesRead) != bytesRead) {
+                perror("Write to destination file failed");
+                close(outFile);
+                close(pipefd[READ_END]);
+                return EXIT_FAILURE;
+            }
+        }
+
+        // Close the read end of the pipe and destination file
+        close(pipefd[READ_END]);
+        close(outFile);
     }
 
- }
- 
+    return EXIT_SUCCESS;
+}
